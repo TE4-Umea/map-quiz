fc6c4ccb43b159fa4e3f2c37f5dc2548ccfd766d
timnilsson04 <timnils04@gmail.com>
2023-11-17 10:57:18 +0100

    ✨

--- src/components/choices.js
+++ src/components/choices.js
@@ -4,7 +4,7 @@ export const textNodes = [
         text: 'https://earth.app.goo.gl/?apn=com.google.earth&isi=293622097&ius=googleearth&link=https%3a%2f%2fearth.google.com%2fweb%2f%4063.8247454,20.2682616,7.26515179a,0d,60y,273.91716745h,83.28991529t,0r%2fdata%3dIjAKLEFGMVFpcFBQcWs5TXQyaU9NQ2dqaVFyQk5tLV9CQUhkdVBCdmdtZnBJZW5vEAU6AwoBMA',
         options: [
             {
-                text: 'picCorrect.png',
+                text: "picCorrect.png",
                 setState: { teknik: true },
                 nextText: 2
             },
